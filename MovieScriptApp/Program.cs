fd9e12ba4c460e4406bf8d3610ad235d22e1961f
cynosureofhyd <tomu646@gmail.com>
2014-01-10 20:39:44 -0700

    Small changes

--- MovieScriptApp/Program.cs
+++ MovieScriptApp/Program.cs
@@ -14,7 +14,7 @@ namespace MovieScriptApp
             string fileToWriteTo = @"C:\Users\PrashMaya\Documents\MovieIds\{0}-{1}.txt";
             string ImdbUrlStringFormat = "http://www.imdb.com/search/title?at=0&languages=en%7C1&sort=moviemeter,asc&start={0}&title_type=feature";
 
-            for(int i = 10000; i < 50000; )
+            for(int i = 21050; i < 50000; )
             {
                 string localFileToWriteTo = String.Format(fileToWriteTo, i, i + 50);
                 string localImdbUrlStringFormat = String.Format(ImdbUrlStringFormat, i + 1);

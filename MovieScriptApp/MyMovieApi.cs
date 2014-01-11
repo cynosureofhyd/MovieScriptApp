d48c458ef6a5841c937f3d3bd2c6c0fe214af61c
cynosureofhyd <tomu646@gmail.com>
2014-01-11 11:11:31 -0700

    Automatically download api information script

---
+++ MovieScriptApp/MyMovieApi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using MovieScriptApp;
+
+namespace ConsoleAppScriptForMovie
+{
+    public class MyMovieApi
+    {
+        public static string Process(string filetowritedownloadeddatato, string movieId)
+        {
+            //System.IO.File.WriteAllLines(@"C:\Users\PrashMaya\Desktop\WriteFirst50Lines.txt", titleIds.ToArray());
+            using (var client = new HttpClient())
+            {
+                string url = "http://mymovieapi.com/?ids={0}&type=json&plot=full&episode=1&lang=en-US&aka=simple&release=simple&business=0&tech=0";
+                string anotherurl = String.Format(url, movieId);
+                client.BaseAddress = new Uri(anotherurl);
+                HttpResponseMessage anotherresponse = client.GetAsync(anotherurl).Result;
+                object obj = JsonConvert.DeserializeObject<object>(anotherresponse.Content.ReadAsStringAsync().Result);
+
+                dynamic moreInfo = JsonConvert.DeserializeObject(obj.ToString());
+                if (obj.ToString() == "[]")
+                    return obj.ToString();
+                string temp = moreInfo[0]["plot"];
+                List<string> tempString = new List<string>();
+                tempString.Add(obj.ToString());
+                System.IO.File.WriteAllLines(filetowritedownloadeddatato, tempString);
+                return obj.ToString();
+            }
+
+            string JSONText = "{Text:\"Hello World!!\", Status:\"Ok\"}";
+            JsonSerializer js = new JsonSerializer();
+            var x = js.Deserialize(new StringReader(JSONText), typeof(HelloWorldResponse)) as HelloWorldResponse;
+        }
+    }
+
+    public class HelloWorldResponse
+    {
+        public string Text { get; set; }
+
+        public string Status { get; set; }
+    }
+}
--- MovieScriptApp/Program.cs
+++ MovieScriptApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConsoleAppScriptForMovie;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +12,67 @@ namespace MovieScriptApp
     {
         static void Main(string[] args)
         {
-
             string fileToWriteTo = @"C:\Users\PrashMaya\Documents\MovieIds\{0}-{1}.txt";
+            string hindiMoviesfileToWriteTo = @"C:\Users\PrashMaya\Documents\HindiMovieIds\{0}-{1}.txt";
+            string teluguMoviesfileToWriteTo = @"C:\Users\PrashMaya\Documents\TeluguMovieIds\{0}-{1}.txt";
+            string hindiImdbUrlStringFormat = "http://www.imdb.com/search/title?at=0&languages=hi%7C1&sort=moviemeter,asc&start={0}&title_type=feature";
+            string teluguImdbUrlStringFormat = "http://www.imdb.com/search/title?at=0&languages=te%7C1&sort=moviemeter,asc&start={0}&title_type=feature";
             string ImdbUrlStringFormat = "http://www.imdb.com/search/title?at=0&languages=en%7C1&sort=moviemeter,asc&start={0}&title_type=feature";
 
-            for(int i = 21050; i < 50000; )
+            //for (int i = 400; i < 3200; )
+            //{
+            //    string localFileToWriteTo = String.Format(teluguMoviesfileToWriteTo, i, i + 50);
+            //    string localImdbUrlStringFormat = String.Format(teluguImdbUrlStringFormat, i + 1);
+            //    string html = DownloadWebPageContent.Download(localImdbUrlStringFormat);
+            //    ParseHtmlForMovieIds.Parse(html, localFileToWriteTo);
+            //    i = i + 50;
+            //}
+
+            string englishMovieFolderPath = @"C:\Users\PrashMaya\Documents\TeluguMovieIds\";
+
+
+            foreach (string file in Directory.EnumerateFiles(englishMovieFolderPath, "*.txt"))
             {
-                string localFileToWriteTo = String.Format(fileToWriteTo, i, i + 50);
-                string localImdbUrlStringFormat = String.Format(ImdbUrlStringFormat, i + 1);
-                string html = DownloadWebPageContent.Download(localImdbUrlStringFormat);
-                ParseHtmlForMovieIds.Parse(html, localFileToWriteTo);
-                i = i + 50;
+                //if (file == "36800-36850.txt")
+                {
+                    string[] lines = File.ReadAllLines(file);
+
+                    
+                    string fileDownloadedMovieInfo = @"C:\Users\PrashMaya\Documents\EnglishMovieMovieApi\{0}.txt";
+                    string teluguFileDownloadedMovieInfo = @"C:\Users\PrashMaya\Documents\TeluguMovieMovieApi\{0}.txt";
+                    string hindiFileDownloadedMovieInfo = @"C:\Users\PrashMaya\Documents\HindiMovieMovieApi\{0}.txt";
+                    foreach (string line in lines)
+                    {
+                        string localFileDownloadedMovieInfo = String.Format(teluguFileDownloadedMovieInfo, line);
+                        // Use a tab to indent each line of the file.
+                        MyMovieApi.Process(localFileDownloadedMovieInfo, line);
+                        //Console.WriteLine("\t" + line);
+                        
+                    }
+                }
             }
 
+            //string fileDownloadedMovieInfo = @"C:\Users\PrashMaya\Documents\EnglishMovieMovieApi\{0}.txt";
+            //fileToWriteTo = String.Format(fileToWriteTo, 2400, 2450);
+            //// Example #2 
+            //// Read each line of the file into a string array. Each element 
+            //// of the array is one line of the file. 
+            //string[] lines = System.IO.File.ReadAllLines(fileToWriteTo);
+            //int i = 0; 
+            
+            //foreach (string line in lines)
+            //{
+            //    string localFileDownloadedMovieInfo = String.Format(fileDownloadedMovieInfo, i);
+            //    // Use a tab to indent each line of the file.
+            //    MyMovieApi.Process(localFileDownloadedMovieInfo, line);
+            //    Console.WriteLine("\t" + line);
+            //    ++i;
+            //}
 
-            //string ImdbUrl = "http://www.imdb.com/search/title?at=0&languages=en%7C1&sort=moviemeter,asc&start=2401&title_type=feature";
-            //string html = DownloadWebPageContent.Download(ImdbUrl);
-            //ParseHtmlForMovieIds.Parse(html, fileToWriteTo);
 
-            //ImdbUrl = "http://www.imdb.com/search/title?at=0&languages=en%7C1&sort=moviemeter,asc&start=2451&title_type=feature";
-            //html = DownloadWebPageContent.Download(ImdbUrl);
-            //ParseHtmlForMovieIds.Parse(html, fileToWriteTo);
+
+
+            
         }
     }
 }
